927ce44e0fef53fa86a6ed243895a27a78a8efe3
Sankalpa Dissanayaka <100987450+sandeepa9803@users.noreply.github.com>
2023-05-25 06:41:08 +0530

    added time

--- client/src/views/NewEvents.jsx
+++ client/src/views/NewEvents.jsx
@@ -48,12 +48,38 @@ export default function NewEvents() {
                     </svg>
                   </div>
                 </div>
+              
                 <div className="ml-4">
                   <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
                     Start Date
                   </h4>
                   <p className="mt-2 dark:text-gray-400">07-05-2023</p>
                 </div>
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+                    <svg
+                      xmlns="./images/calender1.png"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className="w-7 h-7"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium leading-6 dark:text-gray-50">
+                    Time
+                  </h4>
+                  <p className="mt-2 dark:text-gray-400">3 pm</p>
+                </div>
+              
               </div>
               <div className="flex">
                 <div className="flex-shrink-0">
