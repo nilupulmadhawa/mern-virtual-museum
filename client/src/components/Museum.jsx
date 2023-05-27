0cccc2e5e5f638a478238a5f858d4b018bfff9b8
Nilupul Madhawa <dnmadawa@gmail.com>
2023-05-27 10:39:19 +0530

    vr museum added

--- client/src/components/Museum.jsx
+++ client/src/components/Museum.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 import ReactStreetview from "react-streetview";
+import { useParams } from "react-router";
 
-const App = () => {
+const Museum = () => {
     const googleMapsApiKey = "AIzaSyB0pAAfd-SgsJm0w0hvzZfg90qfXoPN9bw";
     const [positions, setpositions] = useState({
         lat: 25.6999017,
         lng: 32.639705
     });
+    const { lat, lng } = useParams()
     useEffect(() => {
-        console.log("POSITIONS", positions);
-    });
+        console.log("POSITIONS", lat, lng)
+        setpositions({
+            lat: parseFloat(lat),
+            lng: parseFloat(lng)
+        });
+    }, []);
+
     const streetViewPanoramaOptions = {
         position: { lat: positions.lat, lng: positions.lng },
         pov: { heading: 100, pitch: 0 },
@@ -21,18 +28,38 @@ const App = () => {
 
     return (
         <div
+            className="bg-dark"
             style={{
                 width: "100%",
                 height: "100vh",
-                backgroundColor: "#eeeeee"
+                justifyContent: "center",
+                display: "flex",
+                alignItems: "center",
             }}
         >
-            <ReactStreetview
-                apiKey={googleMapsApiKey}
-                streetViewPanoramaOptions={streetViewPanoramaOptions}
-            />
-        </div>
+            <div
+                className="bg-gray-800"
+                style={{
+                    width: "90%",
+                    height: "90%",
+                    justifyContent: "center",
+                    display: "flex",
+                    alignItems: "center",
+                }}
+            >
+                <div
+                    style={{
+                        width: "80%",
+                        height: "80%",
+                    }}>
+                    <ReactStreetview
+                        apiKey={googleMapsApiKey}
+                        streetViewPanoramaOptions={streetViewPanoramaOptions}
+                    />
+                </div>
+            </div>
+        </div >
     );
 };
 
-export default App;
+export default Museum;
--- client/src/components/dashboard/museummanage/MuseumDetailsTable.jsx
+++ client/src/components/dashboard/museummanage/MuseumDetailsTable.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import AddMuseumModal from './AddMuseumModal';
 import EditMuseumModal from './EditMuseumModal';
 import { getAllMuseums } from '../../../services/museum';
+import DeleteMuseumModal from './DeleteMuseumModal';
+import { Link } from 'react-router-dom';
 
 export default function MuseumDetailsTable() {
     const [open, setOpen] = useState(false);
@@ -121,43 +123,31 @@ export default function MuseumDetailsTable() {
                                         </td>
                                         <td className="px-3 text-center  py-4 whitespace-no-wrap text-right border-b border-gray-500 text-sm leading-5">
                                             <div className="flex item-center justify-center">
-                                                <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                    >
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                                        />
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                                        />
-                                                    </svg>
-                                                </div>
+                                                <Link to={`/museum/${row.lat}/${row.lng}}`} rel="noreferrer" target={'_blank'} >
+                                                    <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            stroke="currentColor"
+                                                        >
+                                                            <path
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round"
+                                                                stroke-width="2"
+                                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                            />
+                                                            <path
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round"
+                                                                stroke-width="2"
+                                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                                            />
+                                                        </svg>
+                                                    </div>
+                                                </Link>
                                                 <EditMuseumModal row={row} getTableData={_getTableData} />
-                                                <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                    >
-                                                        <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                                                        />
-                                                    </svg>
-                                                </div>
+                                                <DeleteMuseumModal row={row} getTableData={_getTableData} />
                                             </div>
                                         </td>
                                     </tr>
@@ -168,6 +158,6 @@ export default function MuseumDetailsTable() {
                     </table>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
